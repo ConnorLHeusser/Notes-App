2435c8d429465976dab687b51eb1cb54f88f460c
Mighty <jadenja05@gmail.com>
2026-02-03 12:54:09 -0400

    added inline documentation for code

--- Backend-Logic/Code.py
+++ Backend-Logic/Code.py
@@ -1,6 +1,7 @@
 notes = input("Enter new notes here (type e to exit): ")
 new_notes = notes
 while (new_notes != "e"):
+    # Loop with an exit condition 'e' to add new inputed notes into a 'new_notes' variable
     print("")
     print(notes)
     print("")
